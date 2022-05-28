3d22c3e118b6b960e0b055104f67a30c2be8f5de
NishantKaushik007 <96112240+NishantKaushik007@users.noreply.github.com>
2022-05-29 01:02:23 +0530

    Added solution - LeetHub

---
+++ 6. Number is sparse or not - GFG/6.-number-is-sparse-or-not.cpp
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+// C++ program to check if n is sparse or not
+#include<iostream>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to check if the number is sparse or not.
+    bool isSparse(int n)
+    {
+        // Your code
+        int prev,next;
+        while(n>0)
+        {
+            prev=n&1;
+            n=n>>1;
+            next=n&1;
+            if(prev==next&&prev==1&&next==1)
+            return 0;
+            else
+            {
+                prev=next;
+                next=n>>1;
+            }
+        }
+        return 1;
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver Code
+int main()
+{
+    int t,n;
+    
+    //testcases
+    cin>>t;
+    while(t--)
+    {
+        //initializing n
+        cin>>n;
+        
+        //printing 1 if isSparse() returns true
+        //else 0
+        Solution ob;
+        if(ob.isSparse(n)){
+            cout << "1" << endl;
+        }
+        else cout << "0" << endl;
+        
+    }
+    return 0;
+}
+  // } Driver Code Ends
