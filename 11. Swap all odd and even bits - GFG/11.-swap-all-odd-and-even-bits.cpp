7cd2b7226687fe82798d4759164bdd2199f2b09b
NishantKaushik007 <96112240+NishantKaushik007@users.noreply.github.com>
2022-05-30 03:03:10 +0530

    Added solution - LeetHub

---
+++ 11. Swap all odd and even bits - GFG/11.-swap-all-odd-and-even-bits.cpp
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to swap odd and even bits.
+    unsigned int swapBits(unsigned int n)
+    {
+    	// Your code here
+    	int x=((n&(0xAAAAAAAA))>>1),y=((n&(0x55555555))<<1);
+    	return x^y;
+
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;//testcases
+	while(t--)
+	{
+		unsigned int n;
+		cin>>n;//input n
+		
+		Solution ob;
+		//calling swapBits() method
+		cout << ob.swapBits(n) << endl;
+	}
+	return 0;
+}  // } Driver Code Ends
