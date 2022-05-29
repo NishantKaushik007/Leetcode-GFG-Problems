ddf31a83400b8c493598fff223f39fe6dd668332
NishantKaushik007 <96112240+NishantKaushik007@users.noreply.github.com>
2022-05-30 03:10:02 +0530

    Added solution - LeetHub

---
+++ 8. Binary To Gray Code equivalent - GFG/8.-binary-to-gray-code-equivalent.cpp
@@ -0,0 +1,39 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    //  Function to find the gray code of given number n
+    int greyConverter(int n)
+    {
+        
+        // Your code here
+       n=n^(n>>1);
+       return n;
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    cin>>t;//testcases
+    while(t--)
+    {
+        cin >> n;//input n
+        Solution ob;
+        //calling greyConverter() function
+        cout << ob.greyConverter(n) << endl;
+    }
+}
+  // } Driver Code Ends
