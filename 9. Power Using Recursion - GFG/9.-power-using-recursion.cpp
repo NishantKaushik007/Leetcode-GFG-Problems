bc55e8496f229ab701fa213e51e54e70d620649b
NishantKaushik007 <96112240+NishantKaushik007@users.noreply.github.com>
2022-06-07 01:57:20 +0530

    Added solution - LeetHub

---
+++ 9. Power Using Recursion - GFG/9.-power-using-recursion.cpp
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include <iostream>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+    //Complete this function
+    long long RecursivePower(int n,int p)
+    {
+        //Your code here
+    if(p==0)
+    return 1;
+    if(p==1)
+    return n;
+    long long temp=RecursivePower(n,p/2);
+    temp=temp*temp;
+    if(p%2==0)
+    return temp;
+    else
+    return temp*n;
+    }
+};
+
+
+// { Driver Code Starts.
+
+
+int main() {
+	int T;
+	cin>>T;//testcases
+	while(T--)
+	{
+	    int n,p;
+	    
+	    //taking n and p as inputs
+	    cin>>n>>p;
+	    
+	    //calling RecursivePower() function
+	    Solution obj;
+	    cout<<obj.RecursivePower(n,p)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
