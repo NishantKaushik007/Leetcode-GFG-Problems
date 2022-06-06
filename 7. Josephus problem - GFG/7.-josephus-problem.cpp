4a32b5e2fd46cfde25d6dd1d5c2a6d21395aa631
NishantKaushik007 <96112240+NishantKaushik007@users.noreply.github.com>
2022-06-07 00:33:04 +0530

    Added solution - LeetHub

--- 7. Josephus problem - GFG/7.-josephus-problem.cpp
+++ 7. Josephus problem - GFG/7.-josephus-problem.cpp
@@ -15,7 +15,11 @@ class Solution
        //Your code here
        if(n==1)
        return 1;
-       return (josephus(n-1,k)+(k-1))%n+1;
+       int x=(josephus(n-1,k)+k)%n;
+       if(x==0)
+       return n;
+       else
+       return x;
     }
 };
 
