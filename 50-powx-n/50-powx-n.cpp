e07c24d287835e9390230f72546a10e0e124338c
NishantKaushik007 <96112240+NishantKaushik007@users.noreply.github.com>
2022-08-07 23:35:54 +0530

    Time: 3 ms (47.73%), Space: 5.9 MB (43.77%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -5,18 +5,12 @@ public:
         long long nn=n;
         if(nn<0)
             nn=-1*nn;
-        while(nn)
+        while(nn>0)
         {
-            if(nn%2==1)
-            {
+            if(nn&1)
                 ans=ans*x;
-                nn=nn-1;
-            }
-            else
-            {
-                x=x*x;
-                nn=nn/2;
-            }
+            x=x*x;
+            nn=nn>>1;
         }
         if(n<0)
             return (double)(1.0)/(double)ans;
