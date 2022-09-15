41316201194677a2070aa2a69da81a0fa0721a4e
NishantKaushik007 <96112240+NishantKaushik007@users.noreply.github.com>
2022-09-15 23:35:31 +0530

    Time: 31 ms (69.76%), Space: 17.9 MB (96.62%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        //int duprow,dupcol;
         for(int row=0;row<9;row++)
         {
             for(int col=0;col<9;col++)
@@ -10,7 +9,6 @@ public:
                 {
                     char c=board[row][col];
                     board[row][col]='.';
-                    //duprow=row,dupcol=col;
                     for(int i=0;i<9;i++)
                     {
                         if(board[row][i]==c)
@@ -21,7 +19,6 @@ public:
                             return false;
                     }
                     board[row][col]=c;
-                    //row=duprow,col=dupcol;
                 }
             }
         }
