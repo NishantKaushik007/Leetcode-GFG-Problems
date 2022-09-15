f8b4785bc08cad8f71c83e7ff5dd004944112194
NishantKaushik007 <96112240+NishantKaushik007@users.noreply.github.com>
2022-09-16 00:12:37 +0530

    Time: 675 ms (13.58%), Space: 92 MB (39.55%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     bool checkValid(vector<vector<int>>& matrix) {
-         for(int row=0;row<matrix.size();row++)
+        int n=matrix.size();
+        for(int row=0;row<n;row++)
         {
-            for(int col=0;col<matrix.size();col++)
-            {
-                if(matrix[row][col]!='.')
-                {
-                    int x=matrix[row][col];
-                    matrix[row][col]=-1;
-                    for(int i=0;i<matrix.size();i++)
-                    {
-                        if(matrix[row][i]==x)
-                            return false;
-                        if(matrix[i][col]==x)
-                            return false;
-                    }
-                    matrix[row][col]=x;
-                }
-            }
+            unordered_set<int>s;
+            for(int col=0;col<n;col++)
+                s.insert(matrix[row][col]);
+            for(int i=1;i<=n;i++)
+                if(s.find(i)==s.end())
+                    return false;
+        }
+        for(int row=0;row<n;row++)
+        {
+            unordered_set<int>s;
+            for(int col=0;col<n;col++)
+                s.insert(matrix[col][row]);
+            for(int i=1;i<=n;i++)
+                if(s.find(i)==s.end())
+                    return false;
         }
         return true;
     }
