b32c7f0816a4c6efabc3c33673f839d95389d11d
Nishant Kaushik <nishant.nishantkaushik.nishant@gmail.com>
2023-09-01 15:39:29 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (89.73%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
-    int countSetBits(int x)
-    {
-        int count=0;
-        while(x!=0)
-        {
-            x=x&(x-1);
-            count++;
-        }
-        return count;
-    }
     vector<int> countBits(int n) {
         vector<int>ans(n+1,0);
-        for(int i=0;i<=n;i++)
+        ans[0]=0;
+        for(int i=1;i<=n;i++)
         {
-            ans[i]=countSetBits(i);
+            ans[i]=ans[i&(i-1)]+1;
         }
         return ans;
     }
