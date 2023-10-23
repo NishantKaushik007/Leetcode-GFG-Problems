e67dda3fd7be5179126c92965364355ecb8f7553
Nishant Kaushik <nishant.nishantkaushik.nishant@gmail.com>
2023-10-23 10:20:45 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (13.31%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
+    bool powOfFour(int ind,int n)
+    {
+        if(pow(4,ind)>n)
+            return false;
+        if(pow(4,ind)==n)
+            return true;
+        return powOfFour(ind+1,n);
+    }
     bool isPowerOfFour(int n) {
         if(n==1)
             return true;
-        for(int i=1;pow(4,i)<=n;i++)
-        {
-            if(pow(4,i)==n)
-                return true;
-        }
-        return false;
+        return powOfFour(1,n);
     }
 };
