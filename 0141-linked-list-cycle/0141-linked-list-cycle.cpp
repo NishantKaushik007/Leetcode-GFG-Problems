3ebe4ae9076c9f8efe29c69270f79ca64a29d76c
Nishant Kaushik <nishant.nishantkaushik.nishant@gmail.com>
2023-11-03 11:16:18 +0530

    Time: 11 ms (34.48%), Space: 8.3 MB (50.47%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -12,13 +12,12 @@ public:
         if(head==NULL||head->next==NULL)
             return false;
         ListNode* slow=head,*fast=head;
-        while(fast!=NULL&&fast->next!=NULL)
-        {
+        do{
             slow=slow->next;
             fast=fast->next->next;
             if(slow==fast)
                 return true;
-        }
+        }while(fast!=NULL&&fast->next!=NULL);
         return false;
     }
 };
