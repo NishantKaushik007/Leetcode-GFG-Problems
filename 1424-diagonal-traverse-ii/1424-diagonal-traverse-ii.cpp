61fea55d8766f92ce43c8b55560d5373d468e3c6
Nishant Kaushik <nishant.nishantkaushik.nishant@gmail.com>
2023-11-22 22:30:50 +0530

    Time: 128 ms (84.40%), Space: 65.9 MB (94.66%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
-        unordered_map<int,vector<int>>groups;
-        for(int i=nums.size()-1;i>=0;i--)
-        {
-            for(int j=0;j<nums[i].size();j++)
-            {
-                int diagonal=i+j;
-                groups[diagonal].push_back(nums[i][j]);
-            }
-        }
+        queue<pair<int,int>>q;
+        q.push({0,0});
         vector<int>ans;
-        int curr=0;
-        while(groups.find(curr)!=groups.end())
+        while(!q.empty())
         {
-            for(int num:groups[curr])
-                ans.push_back(num);
-            curr++;
+            auto [row,col]=q.front();
+            q.pop();
+            ans.push_back(nums[row][col]);
+            if(col==0&&row+1<nums.size())
+            {
+                q.push({row+1,col});
+            }
+            if(col+1<nums[row].size())
+            {
+                q.push({row,col+1});
+            }
         }
         return ans;
     }
