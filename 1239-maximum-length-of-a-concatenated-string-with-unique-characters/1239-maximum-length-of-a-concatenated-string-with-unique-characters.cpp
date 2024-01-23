219d7730b30bfbdde9dcb96d67f3c653ddae33fb
Nishant Kaushik <nishant.nishantkaushik.nishant@gmail.com>
2024-01-23 13:21:02 +0530

    Time: 8 ms (84.18%), Space: 15 MB (68.25%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool hasDuplicate(string &s1,string &s2)
+    {
+        int arr[26]={0};
+        for(int i=0;i<s1.length();i++)
+        {
+            if(arr[s1[i]-'a']>0)
+                return true;
+            arr[s1[i]-'a']++;
+        }
+        for(int i=0;i<s2.length();i++)
+        {
+            if(arr[s2[i]-'a']>0)
+                return true;
+            arr[s2[i]-'a']--;
+        }
+        return false;
+    }
+    int solve(int ind,vector<string>& arr,string temp,int n)
+    {
+        if(ind>=n)
+            return temp.length();
+        int include=0;
+        int exclude=0;
+        if(hasDuplicate(arr[ind],temp))
+        {
+            exclude=solve(ind+1,arr,temp,n);
+        }
+        else{
+            exclude=solve(ind+1,arr,temp,n);
+            include=solve(ind+1,arr,temp+arr[ind],n);
+        }
+        return max(include,exclude);
+    }
+    int maxLength(vector<string>& arr) {
+        string temp="";
+        int n=arr.size(),i=0;
+        return solve(i,arr,temp,n);
+    }
+};
