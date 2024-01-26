95f5336bc0d683529d10b8bb755e95975e615719
Nishant Kaushik <nishant.nishantkaushik.nishant@gmail.com>
2024-01-26 15:56:24 +0530

    Time: 54 ms (11.85%), Space: 102.1 MB (5.02%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
@@ -3,25 +3,26 @@ public:
     int mod=1e9+7;
     int delRow[4]={-1,0,+1,0};
     int delCol[4]={0,+1,0,-1};
-    int solve(int startRow,int startColumn,int maxMove,int m,int n,vector<vector<vector<int>>> &dp)
-    {
-        if(startRow<0||startRow>=m||startColumn<0||startColumn>=n)
-            return 1;
-        if(maxMove<=0)
-            return 0;
-        if(dp[startRow][startColumn][maxMove]!=-1)
-            return dp[startRow][startColumn][maxMove];
-        int result=0;
-        for(int i=0;i<4;i++)
-        {
-            int nrow=startRow+delRow[i];
-            int ncol=startColumn+delCol[i];
-            result=(result+solve(nrow,ncol,maxMove-1,m,n,dp))%mod;
-        }
-        return dp[startRow][startColumn][maxMove]=result;
-    }
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        vector<vector<vector<int>>>dp(51,vector<vector<int>>(51,vector<int>(51,-1)));
-        return solve(startRow,startColumn,maxMove,m,n,dp);
+        vector<vector<vector<int>>>dp(50,vector<vector<int>>(50,vector<int>(51,0)));
+        for(int k=1;k<=maxMove;k++)
+        {
+            for(int i=0;i<m;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    for(int d=0;d<4;d++)
+                    {
+                        int nrow=i+delRow[d];
+                        int ncol=j+delCol[d];
+                        if(nrow<0||nrow>=m||ncol<0||ncol>=n)
+                            dp[i][j][k]=(dp[i][j][k]+1)%mod;
+                        else
+                            dp[i][j][k]=(dp[i][j][k]+dp[nrow][ncol][k-1])%mod;
+                    }
+                }
+            }
+        }
+        return dp[startRow][startColumn][maxMove];
     }
 };
