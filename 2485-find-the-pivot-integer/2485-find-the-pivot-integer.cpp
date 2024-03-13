3a85b62bfcd5ba6c9c5b8537d1a42ba86d82cf1b
Nishant Kaushik <nishant.nishantkaushik.nishant@gmail.com>
2024-03-13 20:44:15 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (73.82%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        for(int i=1;i<=n;i++)
+        int low=1,high=n;
+        while(low<=high)
         {
-            if((i*(i+1)/2)==(((n*(n+1)/2)-(i*(i+1)/2))+i))
-                return i;
+            int mid=(low+high)/2;
+            if((mid*(mid+1)/2)==(((n*(n+1)/2)-(mid*(mid+1)/2))+mid))
+                return mid;
+            else if((mid*(mid+1)/2)<(((n*(n+1)/2)-(mid*(mid+1)/2))+mid))
+                low=mid+1;
+            else
+                high=mid-1;
         }
         return -1;
     }
