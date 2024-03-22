b2b7630b9772855c301f03fc00f6ef57d3078a1e
Nishant Kaushik <nishant.nishantkaushik.nishant@gmail.com>
2024-03-23 01:13:26 +0530

    Time: 12 ms (44.61%), Space: 12.8 MB (24.45%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char,int>m;
         int ans=0;
+        unordered_map<char,int>m;
         m['I']=1,m['V']=5,m['X']=10,m['L']=50,m['C']=100,m['D']=500,m['M']=1000;
         for(int i=0;i<s.length();i++)
         {
