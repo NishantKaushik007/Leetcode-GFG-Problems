e92c29359e5d924e0a1c3ad460fd26997f338d49
Nishant Kaushik <nishant.nishantkaushik.nishant@gmail.com>
2024-03-24 22:50:11 +0530

    Time: 17 ms (25.63%), Space: 15.9 MB (81.61%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -15,13 +15,9 @@ public:
         TreeNode* left=lowestCommonAncestor(root->left,p,q);
         TreeNode* right=lowestCommonAncestor(root->right,p,q);
         if(left==NULL)
-        {
             return right;
-        }
         else if(right==NULL)
-        {
             return left;
-        }
         else
             return root;
     }
