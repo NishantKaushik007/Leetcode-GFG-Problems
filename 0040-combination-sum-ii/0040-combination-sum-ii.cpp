f075e750c2caf0187d7f8f803ad7279999801f8a
Nishant Kaushik <nishant.nishantkaushik.nishant@gmail.com>
2024-03-29 22:19:29 +0530

    Time: 4 ms (60.66%), Space: 12.6 MB (53.69%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -11,14 +11,11 @@ public:
         {
             if(i>ind&&candidates[i]==candidates[i-1])
                 continue;
-            if(candidates[i]<=target)
-            {
-                sum.push_back(candidates[i]);
-                solve(i+1,target-candidates[i],candidates,sum,ans);
-                sum.pop_back();
-            }
-            else
+            if(candidates[i]>target)
                 break;
+            sum.push_back(candidates[i]);
+            solve(i+1,target-candidates[i],candidates,sum,ans);
+            sum.pop_back();
         }
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
