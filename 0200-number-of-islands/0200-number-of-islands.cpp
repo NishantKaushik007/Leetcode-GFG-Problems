3c1db76f7b2a8f1e32e13ef94ea78df34cd0a45a
Nishant Kaushik <nishant.nishantkaushik.nishant@gmail.com>
2024-03-29 20:10:25 +0530

    Time: 27 ms (70.97%), Space: 23.6 MB (19.60%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
-    void dfs(int row,int col,vector<vector<int>>& vis,vector<vector<char>>& grid,int delrow[],int delcol[])
+    void bfs(int row,int col,vector<vector<int>>& vis,vector<vector<char>>& grid,int delrow[],int delcol[])
     {
+        queue<pair<int,int>>q;
+        q.push({row,col});
         vis[row][col]=1;
-        for(int i=0;i<4;i++)
+        while(!q.empty())
         {
-            int nrow=row+delrow[i];
-            int ncol=col+delcol[i];
-            if(nrow>=0&&nrow<grid.size()&&ncol>=0&&ncol<grid[0].size()&&!vis[nrow][ncol]&&grid[nrow][ncol]=='1')
-                dfs(nrow,ncol,vis,grid,delrow,delcol);
+            int r=q.front().first;
+            int c=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++)
+            {
+                int nrow=r+delrow[i];
+                int ncol=c+delcol[i];
+                if(nrow>=0&&nrow<grid.size()&&ncol>=0&&ncol<grid[0].size()&&!vis[nrow][ncol]&&grid[nrow][ncol]=='1')
+                {
+                    vis[nrow][ncol]=1;
+                    q.push({nrow,ncol});
+                }
+            }
         }
     }
     int numIslands(vector<vector<char>>& grid) {
@@ -23,7 +34,7 @@ public:
                 if(grid[i][j]=='1'&&!vis[i][j])
                 {
                     count++;
-                    dfs(i,j,vis,grid,delrow,delcol);
+                    bfs(i,j,vis,grid,delrow,delcol);
                 }
                     
             }
