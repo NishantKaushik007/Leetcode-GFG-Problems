6382059ae854c479e48aa27aaff4a1926b01dcae
Nishant Kaushik <nishant.nishantkaushik.nishant@gmail.com>
2024-04-02 16:00:48 +0530

    Time: 7 ms (59.26%), Space: 12.6 MB (38.45%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -3,15 +3,13 @@ public:
     int pathSum(int row,int col,vector<vector<int>>& grid,vector<vector<int>>& dp)
     {
         if(row==0&&col==0)
-            return grid[0][0];
+            return grid[row][col];
+        if(row<0||col<0)
+            return 1e8;
         if(dp[row][col]!=-1)
             return dp[row][col];
-        int up=1e8;
-        if(row-1>=0)
-            up=grid[row][col]+pathSum(row-1,col,grid,dp);
-        int left=1e8;
-        if(col-1>=0)
-            left=grid[row][col]+pathSum(row,col-1,grid,dp);
+        int up=grid[row][col]+pathSum(row-1,col,grid,dp);
+        int left=grid[row][col]+pathSum(row,col-1,grid,dp);
         return dp[row][col]=min(up,left);
     }
     int minPathSum(vector<vector<int>>& grid) {
