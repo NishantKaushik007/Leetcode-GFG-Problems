3a2a9c324735d2d8ac2c0145b1010c60caf3d820
Nishant Kaushik <nishant.nishantkaushik.nishant@gmail.com>
2024-04-03 23:49:42 +0530

    Time: 8 ms (47.94%), Space: 17.2 MB (22.94%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -3,13 +3,12 @@ public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n=image.size(),m=image[0].size();
         vector<vector<int>>ans=image,vis(n,vector<int>(m,0));
-        int delRow[]={1,0,-1,0};
-        int delCol[]={0,1,0,-1};
         queue<pair<int,int>>q;
         q.push({sr,sc});
-        vis[sr][sc]=1;
         ans[sr][sc]=color;
-        int orignal=image[sr][sc];
+        int iniCol=image[sr][sc];
+        int delrow[]={1,0,-1,0};
+        int delcol[]={0,1,0,-1};
         while(!q.empty())
         {
             int row=q.front().first;
@@ -17,13 +16,13 @@ public:
             q.pop();
             for(int i=0;i<4;i++)
             {
-                int nrow=row+delRow[i];
-                int ncol=col+delCol[i];
-                if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&!vis[nrow][ncol]&&image[nrow][ncol]==orignal)
+                int nrow=row+delrow[i];
+                int ncol=col+delcol[i];
+                if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&image[nrow][ncol]==iniCol&&!vis[nrow][ncol])
                 {
                     vis[nrow][ncol]=1;
-                    ans[nrow][ncol]=color;
                     q.push({nrow,ncol});
+                    ans[nrow][ncol]=color;
                 }
             }
         }
