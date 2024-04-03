1503596939618d6be4ec5ef002b1f974b64472c0
Nishant Kaushik <nishant.nishantkaushik.nishant@gmail.com>
2024-04-03 11:49:13 +0530

    Time: 12 ms (85.32%), Space: 16.5 MB (97.85%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int>indegree(numCourses,0);
-        vector<int> adj[numCourses];
-        for(auto it:prerequisites)
+        vector<int>vis(numCourses,0),adj[numCourses],indegree(numCourses,0);
+        for(int i=0;i<prerequisites.size();i++)
         {
-            int u=it[1];
-            int v=it[0];
-            indegree[v]++;
+            int u=prerequisites[i][0];
+            int v=prerequisites[i][1];
             adj[u].push_back(v);
+            indegree[v]++;
         }
         queue<int>q;
         for(int i=0;i<numCourses;i++)
@@ -16,6 +15,7 @@ public:
             if(indegree[i]==0)
             {
                 q.push(i);
+                vis[i]=1;
             }
         }
         while(!q.empty())
@@ -24,16 +24,20 @@ public:
             q.pop();
             for(int it:adj[node])
             {
-                indegree[it]--;
-                if(indegree[it]==0)
-                q.push(it);
+                if(!vis[it])
+                {
+                    indegree[it]--;
+                    if(indegree[it]==0)
+                    {
+                        q.push(it);
+                        vis[it]=1;
+                    }
+                }
             }
         }
         for(int i=0;i<numCourses;i++)
-        {
-            if(indegree[i]==1)
+            if(!vis[i])
                 return false;
-        }
         return true;
     }
 };
