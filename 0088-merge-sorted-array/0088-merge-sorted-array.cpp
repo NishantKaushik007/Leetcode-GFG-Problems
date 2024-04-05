2d0f88d46ca77b518ca515ecaf0ce7b45a173287
Nishant Kaushik <nishant.nishantkaushik.nishant@gmail.com>
2024-04-05 14:31:51 +0530

    Time: 2 ms (48.10%), Space: 10.9 MB (95.38%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -4,14 +4,21 @@ public:
         int size=nums1.size()-1,size1=m-1,size2=n-1;
         while(size1>=0&&size2>=0)
         {
-            if(nums1[size1]>nums2[size2])
+            if(nums1[size1]>=nums2[size2])
+            {
                 nums1[size--]=nums1[size1--];
-            else
+            }
+            else{
                 nums1[size--]=nums2[size2--];
+            }
         }
         while(size1>=0)
+        {
             nums1[size--]=nums1[size1--];
+        }
         while(size2>=0)
+        {
             nums1[size--]=nums2[size2--];
+        }
     }
 };
