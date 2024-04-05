5287e7edf5eea025fd8afca903cd3bc21edd6438
Nishant Kaushik <nishant.nishantkaushik.nishant@gmail.com>
2024-04-05 23:53:26 +0530

    Time: 4 ms (41.50%), Space: 13.4 MB (96.90%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -9,7 +9,7 @@ public:
                 return mid;
             else if(nums[low]<=nums[mid])
             {
-                if(target>=nums[low]&&target<=nums[mid])
+                if(target<=nums[mid]&&target>=nums[low])
                     high=mid-1;
                 else
                     low=mid+1;
