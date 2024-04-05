02751028b61b9506ecc9e8a18bdf9641226b687f
Nishant Kaushik <nishant.nishantkaushik.nishant@gmail.com>
2024-04-05 23:46:36 +0530

    Time: 15 ms (85.78%), Space: 24.8 MB (14.18%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -6,9 +6,12 @@ public:
         {
             int mid=(low+high)/2;
             if(nums[mid]==nums[mid^1])
+            {
                 low=mid+1;
-            else
+            }
+            else{
                 high=mid-1;
+            }
         }
         return nums[low];
     }
