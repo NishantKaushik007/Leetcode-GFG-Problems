f6b467596eb9b7cb9b1d01eec6dcb0b1f3d57725
Nishant Kaushik <nishant.nishantkaushik.nishant@gmail.com>
2024-04-18 14:24:13 +0530

    Time: 111 ms (8.29%), Space: 104.3 MB (13.94%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -2,29 +2,19 @@ class Solution {
 public:
     int delRow[4]={1,0,-1,0};
     int delCol[4]={0,1,0,-1};
-    int bfs(int sr,int sc,vector<vector<int>>& grid,vector<vector<int>>& vis)
+    int dfs(int sr,int sc,vector<vector<int>>& grid,vector<vector<int>>& vis,int& count)
     {
-        int count=0;
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
         vis[sr][sc]=1;
-        while(!q.empty())
+        for(int i=0;i<4;i++)
         {
-            int row=q.front().first;
-            int col=q.front().second;
-            q.pop();
-            for(int i=0;i<4;i++)
+            int nrow=sr+delRow[i];
+            int ncol=sc+delCol[i];
+            if(nrow<grid.size()&&nrow>=0&&ncol>=0&&ncol<grid[0].size()&&!vis[nrow][ncol]&&grid[nrow][ncol]==1)
             {
-                int nrow=row+delRow[i];
-                int ncol=col+delCol[i];
-                if(nrow<grid.size()&&nrow>=0&&ncol>=0&&ncol<grid[0].size()&&!vis[nrow][ncol]&&grid[nrow][ncol]==1)
-                {
-                    q.push({nrow,ncol});
-                    vis[nrow][ncol]=1;
-                }
-                else if(nrow>=grid.size()||nrow<0&&ncol<0||ncol>=grid[0].size()||grid[nrow][ncol]==0)
-                    count++;
+                dfs(nrow,ncol,grid,vis,count);
             }
+            else if(nrow>=grid.size()||nrow<0&&ncol<0||ncol>=grid[0].size()||grid[nrow][ncol]==0)
+                count++;
         }
         return count;
     }
@@ -36,7 +26,8 @@ public:
             {
                 if(grid[i][j]==1&&!vis[i][j])
                 {
-                    return bfs(i,j,grid,vis);
+                    int count=0;
+                    return dfs(i,j,grid,vis,count);
                 }
             }
         }
