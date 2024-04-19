762d3879af86624ba763e82fed0bb3701b30b4f2
Nishant Kaushik <nishant.nishantkaushik.nishant@gmail.com>
2024-04-19 10:23:32 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (16.67%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -4,9 +4,10 @@ public:
         int n=haystack.length(),m=needle.length();
         if(m>n)
             return -1;
-        for(int i=0;i<n-m+1;i++)
+        for(int i=0;i<=n-m;i++)
         {
-            if(haystack.substr(i,m)==needle)
+            string s=haystack.substr(i,m);
+            if(needle==s)
                 return i;
         }
         return -1;
