8329b96bc0eb9408966996a8b3ea4dadae5ef905
Nishant Kaushik <nishant.nishantkaushik.nishant@gmail.com>
2024-04-19 23:29:14 +0530

    Time: 7 ms (44.77%), Space: 9.2 MB (81.82%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    bool isSafe(int row,int col,int n,vector<string>& board)
+    bool isSafe(int row,int col,vector<string>& board)
     {
         for(int i=col;i>=0;i--)
+        {
             if(board[row][i]=='Q')
                 return false;
+        }
         int r=row,c=col;
         while(r>=0&&c>=0)
         {
-            if(board[r][c]=='Q')
+            if(board[r--][c--]=='Q')
                 return false;
-            r--;
-            c--;
         }
         r=row,c=col;
-        while(r<n&&c>=0)
+        while(r<board.size()&&c>=0)
         {
-            if(board[r][c]=='Q')
+            if(board[r++][c--]=='Q')
                 return false;
-            r++;
-            c--;
         }
         return true;
     }
@@ -32,7 +30,7 @@ public:
         }
         for(int row=0;row<n;row++)
         {
-            if(isSafe(row,col,n,board))
+            if(isSafe(row,col,board))
             {
                 board[row][col]='Q';
                 placeQueen(col+1,n,board,ans);
