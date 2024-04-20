392192db22c3726231f0755e94a0c6eb9a56420d
Nishant Kaushik <nishant.nishantkaushik.nishant@gmail.com>
2024-04-20 15:24:25 +0530

    Time: 158 ms (37.46%), Space: 88.4 MB (36.73%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -2,28 +2,18 @@ class Solution {
 public:
     int delrow[4]={1,0,-1,0};
     int delcol[4]={0,1,0,-1};
-    pair<int,int> bfs(int row,int col,vector<vector<int>>& vis,vector<vector<int>>& land)
+    pair<int,int> dfs(int row,int col,vector<vector<int>>& vis,vector<vector<int>>& land,int& maxrow,int& maxcol)
     {
-        queue<pair<int,int>>q;
-        q.push({row,col});
         vis[row][col]=1;
-        int maxrow=row,maxcol=col;
-        while(!q.empty())
+        maxrow=max(maxrow,row);
+        maxcol=max(maxcol,col);
+        for(int i=0;i<4;i++)
         {
-            int r=q.front().first;
-            int c=q.front().second;
-            q.pop();
-            for(int i=0;i<4;i++)
+            int nrow=row+delrow[i];
+            int ncol=col+delcol[i];
+            if(nrow<land.size()&&nrow>=0&&ncol>=0&&ncol<land[0].size()&&!vis[nrow][ncol]&&land[nrow][ncol]==1)
             {
-                int nrow=r+delrow[i];
-                int ncol=c+delcol[i];
-                if(nrow<land.size()&&nrow>=0&&ncol>=0&&ncol<land[0].size()&&!vis[nrow][ncol]&&land[nrow][ncol]==1)
-                {
-                    q.push({nrow,ncol});
-                    vis[nrow][ncol]=1;
-                    maxrow=max(maxrow,nrow);
-                    maxcol=max(maxcol,ncol);
-                }
+                dfs(nrow,ncol,vis,land,maxrow,maxcol);
             }
         }
         return {maxrow,maxcol};
@@ -38,9 +28,10 @@ public:
                 if(!vis[i][j]&&land[i][j]==1)
                 {
                     vector<int>coord;
-                    pair<int,int>x=bfs(i,j,vis,land);
                     coord.push_back(i);
                     coord.push_back(j);
+                    int maxrow=0,maxcol=0;
+                    pair<int,int>x=dfs(i,j,vis,land,maxrow,maxcol);
                     coord.push_back(x.first);
                     coord.push_back(x.second);
                     ans.push_back(coord);
