5b2ce5f037a1ca45efd99ff89f93672bd0b6be1e
Nishant Kaushik <nishant.nishantkaushik.nishant@gmail.com>
2024-04-24 09:53:55 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (13.08%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    int tribonacci(int n) {
-        int a=0,b=1,c=1;
-        if(n==0)
-            return 0;
+    int tri(int n,vector<int>& dp)
+    {
         if(n==1||n==2)
             return 1;
-        for(int i=3;i<=n;i++)
-        {
-            int d=a+b+c;
-            a=b;
-            b=c;
-            c=d;
-        }
-        return c;
+        if(n==0)
+            return 0;
+        if(dp[n]!=-1)
+            return dp[n];
+        return dp[n]=tri(n-1,dp)+tri(n-2,dp)+tri(n-3,dp);
+    }
+    int tribonacci(int n) {
+        vector<int>dp(n+1,-1);
+        return tri(n,dp);
     }
 };
