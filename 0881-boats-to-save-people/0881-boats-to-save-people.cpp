320927acb22e327f618c281e49c501796f50b365
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2024-05-04 08:58:24 +0530

    Time: 47 ms (93.77%), Space: 51.3 MB (8.97%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-        sort(people.begin(),people.end());
-        int i=0,j=people.size()-1,boat=0;
-        if(people[j]>limit)
+        vector<int>freq(limit+1,0),v;
+        for(int i=0;i<people.size();i++)
+        {
+            freq[people[i]]++;
+        }
+        for(int i=1;i<=limit;i++)
+        {
+            while(freq[i]!=0)
+            {
+                v.push_back(i);
+                freq[i]--;
+            }
+        }
+        int i=0,j=v.size()-1,boat=0;
+        if(v[j]>limit)
             return -1;
         while(i<=j)
         {
-            if(people[i]+people[j]<=limit)
+            if(v[i]+v[j]<=limit)
             {
                 boat++;
                 i++;
                 j--;
             }
-            else if(people[j]<=limit)
+            else if(v[j]<=limit)
             {
                 boat++;
                 j--;
