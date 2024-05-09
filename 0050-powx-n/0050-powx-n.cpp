5cacb713fe4bc0fa586a87cb59b941e6f2f523f4
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2024-05-09 21:39:06 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (21.38%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -4,16 +4,16 @@ public:
         double ans=1.0;
         long long nn=n;
         if(nn<0)
-            nn=nn*-1;
+            nn=-1*nn;
         while(nn)
         {
             if(nn&1)
                 ans*=x;
-            x=x*x;
+            x*=x;
             nn=nn>>1;
         }
         if(n<0)
-            return (double)(1.0)/ans;
+            return double(1.0)/ans;
         return ans;
     }
 };
