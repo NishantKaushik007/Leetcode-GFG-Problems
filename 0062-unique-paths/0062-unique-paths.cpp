d2871eae0873d9159320eca5a9981831bc0a9fb6
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2024-05-09 21:54:53 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (86.54%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         double ans=1;
-        int N=m+n-2,r=m-1;
+        int N=m+n-2,r=min(m,n)-1;
         for(int i=1;i<=r;i++)
-           ans=ans*(N-r+i)/i;
+        {
+            ans=ans*(N-r+i)/i;
+        }
         return ans;
     }
 };
