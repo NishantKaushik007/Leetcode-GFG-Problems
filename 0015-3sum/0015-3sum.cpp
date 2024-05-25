f3b4b28c51c5fbaf58f62945146233fac8428a34
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2024-05-25 12:47:29 +0530

    Time: 68 ms (54.10%), Space: 29.1 MB (52.89%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,29 +5,29 @@ public:
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size();i++)
         {
-            if(i>0&&nums[i]==nums[i-1])
+            if(i!=0&&nums[i]==nums[i-1])
                 continue;
-            int j=i+1,k=nums.size()-1;
-            while(j<k)
+            int l=i+1,h=nums.size()-1;
+            while(l<h)
             {
                 int sum=nums[i];
-                sum+=nums[j];
-                sum+=nums[k];
+                sum+=nums[l];
+                sum+=nums[h];
                 if(sum==0)
                 {
-                    vector<int>v={nums[i],nums[j],nums[k]};
+                    vector<int>v={nums[i],nums[l],nums[h]};
                     ans.push_back(v);
-                    j++;
-                    k--;
-                    while(j<k&&nums[j]==nums[j-1])
-                        j++;
-                    while(j<k&&nums[k]==nums[k+1])
-                        k--;
+                    l++;
+                    h--;
+                    while(l<h&&nums[l]==nums[l-1])
+                        l++;
+                    while(l<h&&nums[h]==nums[h+1])
+                        h--;
                 }
-                else if(sum<0)
-                    j++;
+                else if(sum>0)
+                    h--;
                 else
-                    k--;
+                    l++;
             }
         }
         return ans;
