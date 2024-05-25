7e1368aea77ef4b234d66aed9da6cd806648a6a5
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2024-05-25 14:06:04 +0530

    Time: 6 ms (76.14%), Space: 21 MB (91.21%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -4,11 +4,8 @@ public:
         int i=0,j=1;
         while(j<nums.size())
         {
-            if(nums[i]!=nums[j])
-            {
-                i++;
-                nums[i]=nums[j];
-            }
+            if(nums[j]!=nums[i])
+                nums[++i]=nums[j];
             j++;
         }
         return i+1;
