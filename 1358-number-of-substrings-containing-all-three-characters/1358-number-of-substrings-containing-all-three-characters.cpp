b3b3e8ad2ba10f4777a9fc9d98cca83499da74d1
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2024-06-17 16:37:09 +0530

    Time: 18 ms (55.59%), Space: 9.6 MB (66.59%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        unordered_map<char,int>mp;
-        int l=0,r=0,count=0;
-        while(r<s.length())
+        int freq[3]={-1,-1,-1},count=0;
+        for(int i=0;i<s.length();i++)
         {
-            mp[s[r]]++;
-            while(mp.size()==3)
-            {
-                count+=s.length()-r;
-                mp[s[l]]--;
-                if(mp[s[l]]==0)
-                    mp.erase(s[l]);
-                l++;
-            }
-            r++;
+            freq[s[i]-'a']=i;
+            count+=min(freq[0],min(freq[1],freq[2]))+1;
         }
         return count;
     }
