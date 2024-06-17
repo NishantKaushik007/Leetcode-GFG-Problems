9b1b9e130e5bc1eb1acefe07623d1d58cdb779d6
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2024-06-17 18:10:40 +0530

    Time: 46 ms (19.86%), Space: 38.1 MB (29.32%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int totalCount = 0;
-        int currentSum = 0;
-        // {prefix: number of occurrence}
-        unordered_map<int, int> freq; // To store the frequency of prefix sums
-
-        for (int num : nums) {
-            currentSum += num;
-            if (currentSum == goal){
-                totalCount++;
-            }
-
-            // Check if there is any prefix sum that can be subtracted from the current sum to get the desired goal
-            if (freq.find(currentSum - goal) != freq.end()){
-                totalCount += freq[currentSum - goal];
-            }
-
-            freq[currentSum]++;
+        unordered_map<int,int>mp;
+        int preSum=0,count=0;
+        mp[0]=1;
+        for(int i=0;i<nums.size();i++)
+        {
+            preSum+=nums[i];
+            int prevOcc=preSum-goal;
+            if(mp.find(prevOcc)!=mp.end())
+                count+=mp[prevOcc];
+            mp[preSum]++;
         }
-
-        return totalCount;
+        return count;
     }
 };
