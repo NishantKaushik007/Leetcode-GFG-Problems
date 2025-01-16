9b23e8d49081c74166098c5485e343b3ae933ab8
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-01-16 20:05:56 +0530

    Time: 3 ms (69.59%), Space: 65.1 MB (63.77%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -6,6 +6,11 @@ public:
             slow=nums[slow];
             fast=nums[nums[fast]];
         }while(slow!=fast);
+        fast=nums[0];
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
         return slow;
     }
 };
