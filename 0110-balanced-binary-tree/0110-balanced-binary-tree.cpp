15a44a3d752a20ebbb89cb203d1dee7b219d4220
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-01-17 21:36:45 +0530

    Time: 0 ms (100%), Space: 22.9 MB (73.1%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,21 +11,21 @@
  */
 class Solution {
 public:
-    int height(TreeNode* root)
+    int solve(TreeNode* root)
     {
         if(root==NULL)
-            return 0;
-        int left=height(root->left);
+        return 0;
+        int left=solve(root->left);
         if(left==-1)
-            return -1;
-        int right=height(root->right);
+        return -1;
+        int right=solve(root->right);
         if(right==-1)
-            return -1;
+        return -1;
         if(abs(left-right)>1)
-            return -1;
+        return -1;
         return 1+max(left,right);
     }
     bool isBalanced(TreeNode* root) {
-        return (height(root)==-1)?false:true;
+        return (solve(root)==-1)?false:true;
     }
 };
