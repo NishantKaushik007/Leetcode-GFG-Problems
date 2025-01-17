f194f81e088385b95db5ff141b9b8207fd870a89
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-01-17 11:40:23 +0530

    Time: 0 ms (100%), Space: 8.7 MB (14.39%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double ans=1.0;
         long long nn=n;
+        double ans=1;
         if(nn<0)
-            nn=-1*nn;
-        while(nn)
-        {
+        nn=-1*nn;
+        while(nn){
             if(nn&1)
-                ans*=x;
-            x*=x;
+            ans*=x;
+            x=x*x;
             nn=nn>>1;
         }
         if(n<0)
-            return double(1.0)/ans;
+        return double(1)/ans;
         return ans;
     }
 };
