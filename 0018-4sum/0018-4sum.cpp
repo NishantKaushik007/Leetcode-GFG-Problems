0cebb2f85e218c8d3ecb52733fe5a4abf2acab03
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-01-17 12:41:09 +0530

    Time: 910 ms (5.65%), Space: 248.6 MB (5%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -3,40 +3,36 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++)
-        {
-            if(i>0&&nums[i]==nums[i-1])
+        for(int i=0;i<nums.size();i++){
+            if(i!=0&&nums[i]==nums[i-1])
+            continue;
+            for(int j=i+1;j<nums.size();j++){
+                if(j!=i+1&&nums[j]==nums[j-1])
                 continue;
-            for(int j=i+1;j<nums.size();j++)
-            {
-                if(j>i+1&&nums[j]==nums[j-1])
-                    continue;
-                int k=j+1,l=nums.size()-1;
-                while(k<l)
-                {
+                int l=j+1,h=nums.size()-1;
+                while(l<h){
                     long long sum=nums[i];
                     sum+=nums[j];
-                    sum+=nums[k];
                     sum+=nums[l];
+                    sum+=nums[h];
                     vector<int>v(4);
-                    if(sum==target)
-                    {
+                    if(sum==target){
                         v[0]=nums[i];
                         v[1]=nums[j];
-                        v[2]=nums[k];
-                        v[3]=nums[l];
+                        v[2]=nums[l];
+                        v[3]=nums[h];
                         ans.push_back(v);
-                        k++;
-                        l--;
-                        while(k<l&&nums[k]==nums[k-1])
-                        k++;
-                        while(k<l&&nums[l]==nums[l+1])
-                        l--;
+                        l++;
+                        h--;
+                        while(l<h&&nums[l]==nums[l-1])
+                        l++;
+                        while(l<h&&nums[h]==nums[h+1])
+                        h--;
                     }
-                    else if(sum<target)
-                        k++;
+                    else if(sum>target)
+                    h--;
                     else
-                        l--;
+                    l++;
                 }
             }
         }
