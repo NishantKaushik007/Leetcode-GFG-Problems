d3b433e7a1eb699925f0063f91cc6e025404abb6
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-01-18 23:27:10 +0530

    Time: 126 ms (54.75%), Space: 16.3 MB (77.48%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -6,20 +6,22 @@ public:
         sum+=num;
         if(sum%2)
         return false;
-        vector<vector<bool>>dp(nums.size(),vector<bool>((sum/2)+1,false));
+        vector<bool>prev((sum/2)+1,false);
         for(int i=0;i<nums.size();i++)
-        dp[i][0]=true;
+        prev[0]=true;
         for(int ind=1;ind<nums.size();ind++)
         {
+            vector<bool>curr((sum/2)+1,false);
             for(int tar=0;tar<=sum/2;tar++)
             {
-                bool notTake=dp[ind-1][tar];
+                bool notTake=prev[tar];
                 bool take=false;
                 if(tar>=nums[ind])
-                take=dp[ind-1][tar-nums[ind]];
-                dp[ind][tar]=take|notTake;
+                take=prev[tar-nums[ind]];
+                curr[tar]=take|notTake;
             }
+            prev=curr;
         }
-        return dp[nums.size()-1][sum/2];
+        return prev[sum/2];
     }
 };
