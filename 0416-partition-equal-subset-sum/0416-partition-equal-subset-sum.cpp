e8b6aa8e27d03adb64748f6acd60346a19338bb8
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-01-18 23:25:44 +0530

    Time: 143 ms (37.44%), Space: 16.9 MB (68.67%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    bool canPart(int ind,int tar,vector<int>& nums,vector<vector<int>>& dp)
-    {
-        if(tar==0)
-        return true;
-        if(ind==0)
-        return tar==nums[0];
-        if(dp[ind][tar]!=-1)
-        return dp[ind][tar];
-        bool notTake=canPart(ind-1,tar,nums,dp);
-        bool take=false;
-        if(tar>=nums[ind])
-        take=canPart(ind-1,tar-nums[ind],nums,dp);
-        return dp[ind][tar]=take|notTake;
-    }
     bool canPartition(vector<int>& nums) {
         int sum=0;
         for(int num:nums)
         sum+=num;
         if(sum%2)
         return false;
-        vector<vector<int>>dp(nums.size(),vector<int>((sum/2)+1,-1));
-        return canPart(nums.size()-1,sum/2,nums,dp);
+        vector<vector<bool>>dp(nums.size(),vector<bool>((sum/2)+1,false));
+        for(int i=0;i<nums.size();i++)
+        dp[i][0]=true;
+        for(int ind=1;ind<nums.size();ind++)
+        {
+            for(int tar=0;tar<=sum/2;tar++)
+            {
+                bool notTake=dp[ind-1][tar];
+                bool take=false;
+                if(tar>=nums[ind])
+                take=dp[ind-1][tar-nums[ind]];
+                dp[ind][tar]=take|notTake;
+            }
+        }
+        return dp[nums.size()-1][sum/2];
     }
 };
