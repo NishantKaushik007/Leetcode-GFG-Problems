38610f0d87b51eb52c4a0e05f507d8772467fe7d
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-01-18 19:01:58 +0530

    Time: 4 ms (7.79%), Space: 13 MB (58.4%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    int minSum(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>& dp)
-    {
-        if(i==triangle.size())
-        return 0;
-        if(dp[i][j]!=-1)
-        return dp[i][j];
-        int down=triangle[i][j]+minSum(i+1,j,triangle,dp);
-        int dia=triangle[i][j]+minSum(i+1,j+1,triangle,dp);
-        return dp[i][j]=min(down,dia);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
-        vector<vector<int>>dp(triangle.size(),vector<int>(triangle.size(),-1));
+        vector<int>ahead(triangle.size(),-1);
         for(int i=0;i<triangle.size();i++)
-        dp[triangle.size()-1][i]=triangle[triangle.size()-1][i];
+        ahead[i]=triangle[triangle.size()-1][i];
         for(int i=triangle.size()-2;i>=0;i--)
         {
+            vector<int>curr(triangle.size(),-1);
             for(int j=0;j<=i;j++)
             {
-                int up=triangle[i][j]+dp[i+1][j];
-                int right=triangle[i][j]+dp[i+1][j+1];
-                dp[i][j]=min(up,right);
+                int up=triangle[i][j]+ahead[j];
+                int right=triangle[i][j]+ahead[j+1];
+                curr[j]=min(up,right);
             }
+            ahead=curr;
         }
-        return dp[0][0];
+        return ahead[0];
     }
 };
