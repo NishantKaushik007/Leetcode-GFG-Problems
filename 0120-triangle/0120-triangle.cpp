9ddeb74463ee227c33b51e446936cd8d5328ff49
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-01-18 18:50:46 +0530

    Time: 0 ms (100%), Space: 13.2 MB (25.35%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>& dp)
+    int minSum(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>& dp)
     {
-        if(i==triangle.size()-1)
-        {
-            return triangle[i][j];
-        }
+        if(i==triangle.size())
+        return 0;
         if(dp[i][j]!=-1)
-            return dp[i][j];
-        int down=triangle[i][j]+solve(i+1,j,triangle,dp);
-        int right=triangle[i][j]+solve(i+1,j+1,triangle,dp);
-        return dp[i][j]=min(down,right);
+        return dp[i][j];
+        int down=triangle[i][j]+minSum(i+1,j,triangle,dp);
+        int dia=triangle[i][j]+minSum(i+1,j+1,triangle,dp);
+        return dp[i][j]=min(down,dia);
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         vector<vector<int>>dp(triangle.size(),vector<int>(triangle.size(),-1));
-        return solve(0,0,triangle,dp);
+        return minSum(0,0,triangle,dp);
     }
 };
