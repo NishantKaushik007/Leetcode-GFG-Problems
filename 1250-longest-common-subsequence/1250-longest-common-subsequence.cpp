793547fc503e36db9b3ed14eec7fe53682246bb9
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-01-20 15:13:54 +0530

    Time: 25 ms (67.54%), Space: 27.6 MB (26.06%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -11,7 +11,17 @@ public:
         return dp[i][j]=max(lcs(i-1,j,text1,text2,dp),lcs(i,j-1,text1,text2,dp));
     }
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>>dp(text1.length(),vector<int>(text2.length(),-1));
-        return lcs(text1.length()-1,text2.length()-1,text1,text2,dp);
+        vector<vector<int>>dp(text1.length()+1,vector<int>(text2.length()+1,0));
+        for(int i=1;i<=text1.length();i++)
+        {
+            for(int j=1;j<=text2.length();j++)
+            {
+                if(text1[i-1]==text2[j-1])
+                dp[i][j]=1+dp[i-1][j-1];
+                else
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[text1.length()][text2.length()];
     }
 };
