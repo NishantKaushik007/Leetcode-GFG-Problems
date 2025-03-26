b4e0ddd4add0aa8dd9055a3856b788cf16e0fe95
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-03-26 16:15:07 +0530

    Time: 0 ms (100%), Space: 8.7 MB (17.13%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
+    int solve(int n,vector<int>& dp)
+    {
+        if(n==0||n==1)
+        return 1;
+        if(dp[n]!=-1)
+        return dp[n];
+        int fs=solve(n-1,dp);
+        int ss=solve(n-2,dp);
+        return dp[n]=fs+ss;
+    }
     int climbStairs(int n) {
-        int a=1,b=1;
-        for(int i=2;i<=n;i++)
-        {
-            int c=a+b;
-            a=b;
-            b=c;
-        }
-        return b;
+        vector<int>dp(n+1,-1);
+        return solve(n,dp);
     }
 };
