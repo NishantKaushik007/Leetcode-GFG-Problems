21c1adf5e48118373a9fb125308b37cc99b378e1
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-03-29 23:33:17 +0530

    Time: 27 ms (29.87%), Space: 17.6 MB (32.31%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,38 +1,42 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
+        vector<vector<int>>ans;
+        for(int i=0;i<nums.size();i++)
+        {
             if(i!=0&&nums[i]==nums[i-1])
             continue;
-            for(int j=i+1;j<nums.size();j++){
+            for(int j=i+1;j<nums.size();j++)
+            {
                 if(j!=i+1&&nums[j]==nums[j-1])
                 continue;
-                int l=j+1,h=nums.size()-1;
-                while(l<h){
+                int low=j+1,high=nums.size()-1;
+                while(low<high)
+                {
                     long long sum=nums[i];
                     sum+=nums[j];
-                    sum+=nums[l];
-                    sum+=nums[h];
-                    vector<int>v(4);
-                    if(sum==target){
+                    sum+=nums[low];
+                    sum+=nums[high];
+                    if(sum==target)
+                    {
+                        vector<int>v(4);
                         v[0]=nums[i];
                         v[1]=nums[j];
-                        v[2]=nums[l];
-                        v[3]=nums[h];
+                        v[2]=nums[low];
+                        v[3]=nums[high];
                         ans.push_back(v);
-                        l++;
-                        h--;
-                        while(l<h&&nums[l]==nums[l-1])
-                        l++;
-                        while(l<h&&nums[h]==nums[h+1])
-                        h--;
+                        low++;
+                        high--;
+                        while(low<high&&nums[low]==nums[low-1])
+                        low++;
+                        while(low<high&&nums[high]==nums[high+1])
+                        high--;
                     }
                     else if(sum>target)
-                    h--;
+                    high--;
                     else
-                    l++;
+                    low++;
                 }
             }
         }
