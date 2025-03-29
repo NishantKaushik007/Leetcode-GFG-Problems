4dd3fdb35f943fa8d979870a81b733deb48b179c
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-03-29 22:04:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int>ans;
-        int c1=0,c2=0,e1=-1,e2=-1;
-        for(int i=0;i<nums.size();i++){
-            if(e1==nums[i])
+        int ele1=-1,ele2=-1,c1=0,c2=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(ele1==nums[i])
             c1++;
-            else if(e2==nums[i])
+            else if(ele2==nums[i])
             c2++;
             else if(c1==0)
             {
-                e1=nums[i];
+                ele1=nums[i];
                 c1=1;
             }
-            else if(c2==0){
-                e2=nums[i];
+            else if(c2==0)
+            {
+                ele2=nums[i];
                 c2=1;
             }
             else{
@@ -23,16 +24,18 @@ public:
             }
         }
         c1=0,c2=0;
-        for(int i=0;i<nums.size();i++){
-            if(e1==nums[i])
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==ele1)
             c1++;
-            else if(e2==nums[i])
+            else if(nums[i]==ele2)
             c2++;
         }
-        if(c1>nums.size()/3)
-        ans.push_back(e1);
-        if(c2>nums.size()/3)
-        ans.push_back(e2);
+        vector<int>ans;
+        if(c1>=nums.size()/3)
+        ans.push_back(ele1);
+        if(c2>=nums.size()/3)
+        ans.push_back(ele2);
         return ans;
     }
 };
