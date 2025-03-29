3a4f02c731f31c484c046e9483858015ec5c148a
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-03-29 18:43:00 +0530

    Time: 0 ms (100%), Space: 23.9 MB (59.55%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,13 +3,12 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>>ans;
         sort(intervals.begin(),intervals.end());
-        ans.push_back(intervals[0]);
-        for(int i=1;i<intervals.size();i++){
-            if(ans.back()[1]>=intervals[i][0]){
-                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
-            }
-            else
+        for(int i=0;i<intervals.size();i++)
+        {
+            if(ans.size()==0||ans.back()[1]<intervals[i][0])
             ans.push_back(intervals[i]);
+            else
+            ans.back()[1]=max(intervals[i][1],ans.back()[1]);
         }
         return ans;
     }
