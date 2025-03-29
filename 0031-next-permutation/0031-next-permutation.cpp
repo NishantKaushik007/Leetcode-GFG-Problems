86156d497933dfac5da2e1f481abf97b73fae563
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-03-29 11:50:39 +0530

    Time: 0 ms (100%), Space: 15.7 MB (33.01%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int i,j;
-        for(i=nums.size()-2;i>=0;i--)
+        int n=nums.size(),i,j;
+        for(i=n-2;i>=0;i--)
         {
             if(nums[i]<nums[i+1])
             break;
@@ -10,13 +10,13 @@ public:
         if(i<0)
         reverse(nums.begin(),nums.end());
         else{
-            for(j=nums.size()-1;j>i;j--){
-                if(nums[j]>nums[i]){
-                    swap(nums[i],nums[j]);
-                    reverse(nums.begin()+i+1,nums.end());
-                    break;
-                }
+            for(j=n-1;j>i;j--)
+            {
+                if(nums[j]>nums[i])
+                break;
             }
+            swap(nums[i],nums[j]);
+            reverse(nums.begin()+i+1,nums.end());
         }
     }
 };
