6caa866a2c574a4d52becc5eb747aba785039e84
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-03-29 22:41:06 +0530

    Time: 590 ms (10.11%), Space: 243.5 MB (33.7%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.cpp
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -1,33 +1,41 @@
 class Solution {
 public:
-    int merge(vector<int>& nums,int low,int mid,int high){
+    int merge(vector<int>& nums,int low,int mid,int high)
+    {
         vector<int>v;
         int left=low,right=mid+1,count=0;
-        while(left<=mid&&right<=high){
-            if(nums[left]>2LL*nums[right]){
+        while(left<=mid&&right<=high)
+        {
+            if(nums[left]>2ll*nums[right])
+            {
                 count+=(mid-left+1);
                 right++;
             }
-            else
-            left++;
+            else{
+                left++;
+            }
         }
         left=low,right=mid+1;
-        while(left<=mid&&right<=high){
+        while(left<=mid&&right<=high)
+        {
             if(nums[left]>nums[right])
-            v.push_back(nums[right++]);
-            else
-            v.push_back(nums[left++]);
+            {
+                v.push_back(nums[right++]);
+            }
+            else{
+                v.push_back(nums[left++]);
+            }
         }
         while(left<=mid)
         v.push_back(nums[left++]);
         while(right<=high)
         v.push_back(nums[right++]);
-        for(int i=low;i<=high;i++){
-            nums[i]=v[i-low];
-        }
+        for(int i=low;i<=high;i++)
+        nums[i]=v[i-low];
         return count;
     }
-    int mergeSort(vector<int>& nums,int low,int high){
+    int mergeSort(vector<int>& nums,int low,int high)
+    {
         if(low>=high)
         return 0;
         int mid=(low+high)/2;
