8ff3769cd4be5ff86f9ada70cd6d2078c9d9453e
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-03-31 22:55:59 +0530

    Time: 0 ms (100%), Space: 12.9 MB (62.59%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        vector<vector<int>>dp(triangle.size(),vector<int>(triangle.size(),0));
+        vector<int>ahead(triangle.size(),0);
         for(int i=0;i<triangle.size();i++)
-        dp[triangle.size()-1][i]=triangle[triangle.size()-1][i];
+        ahead[i]=triangle[triangle.size()-1][i];
         for(int i=triangle.size()-2;i>=0;i--)
         {
+            vector<int>curr(triangle.size(),0);
             for(int j=i;j>=0;j--)
             {
-                int down=triangle[i][j]+dp[i+1][j];
-                int right=triangle[i][j]+dp[i+1][j+1];
-                dp[i][j]=min(down,right);
+                int down=triangle[i][j]+ahead[j];
+                int right=triangle[i][j]+ahead[j+1];
+                curr[j]=min(down,right);
             }
+            ahead=curr;
         }
-        return dp[0][0];
+        return ahead[0];
     }
 };
