3e2e72173732b5a0ed39c593b65ba9299b2314d7
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-03-31 17:25:44 +0530

    Time: 0 ms (100%), Space: 10.9 MB (40.52%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int robbed(vector<int>& nums) {
-        int prev=nums[0],prev2=0;
+        int prev1=nums[0],prev2=0;
         for(int ind=1;ind<nums.size();ind++)
         {
-            int notTake=prev;
             int take=nums[ind];
             if(ind>1)
             take+=prev2;
+            int notTake=0+prev1;
             int curr=max(take,notTake);
-            prev2=prev;
-            prev=curr;
+            prev2=prev1;
+            prev1=curr;
         }
-        return prev;
+        return prev1;
     }
     int rob(vector<int>& nums) {
         if(nums.size()==1)
         return nums[0];
-        vector<int>v1(nums.begin(),nums.end()-1),v2(nums.begin()+1,nums.end());
-        return max(robbed(v1),robbed(v2));
+        vector<int>num1(nums.begin(),nums.end()-1),num2(nums.begin()+1,nums.end());
+        return max(robbed(num1),robbed(num2));
     }
 };
