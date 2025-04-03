3ef38fa3dbcbe1a4262039c66322291adc46e0fb
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-04-04 00:49:07 +0530

    Time: 2 ms (74.99%), Space: 10.2 MB (56.15%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,31 +1,6 @@
 class Solution {
 public:
-    bool isSafe(int row,int col,vector<string>& board,int n)
-    {
-        for(int i=col;i>=0;i--)
-        {
-            if(board[row][i]=='Q')
-            return false;
-        }
-        int nrow=row-1,ncol=col-1;
-        while(nrow>=0&&ncol>=0)
-        {
-            if(board[nrow][ncol]=='Q')
-            return false;
-            nrow--;
-            ncol--;
-        }
-        nrow=row+1,ncol=col-1;
-        while(nrow<n&&ncol>=0)
-        {
-            if(board[nrow][ncol]=='Q')
-            return false;
-            nrow++;
-            ncol--;
-        }
-        return true;
-    }
-    void placeQueen(int col,vector<string>& board,vector<vector<string>>& ans,int n)
+    void placeQueen(int col,vector<string>& board,vector<vector<string>>& ans,int n,vector<int>& r,vector<int>& udia,vector<int>& ddia)
     {
         if(col==n)
         {
@@ -34,11 +9,17 @@ public:
         }
         for(int i=0;i<n;i++)
         {
-            if(isSafe(i,col,board,n))
+            if(r[i]==0&&udia[n-1+col-i]==0&&ddia[i+col]==0)
             {
                 board[i][col]='Q';
-                placeQueen(col+1,board,ans,n);
+                r[i]=1;
+                udia[n-1+col-i]=1;
+                ddia[i+col]=1;
+                placeQueen(col+1,board,ans,n,r,udia,ddia);
                 board[i][col]='.';
+                r[i]=0;
+                udia[n-1+col-i]=0;
+                ddia[i+col]=0;
             }
         }
     }
@@ -48,7 +29,10 @@ public:
         string s(n,'.');
         for(int i=0;i<n;i++)
         board[i]=s;
-        placeQueen(0,board,ans,n);
+        vector<int>r(n,0);
+        vector<int>udia(2*n-1,0);
+        vector<int>ddia(2*n-1,0);
+        placeQueen(0,board,ans,n,r,udia,ddia);
         return ans;
     }
 };
