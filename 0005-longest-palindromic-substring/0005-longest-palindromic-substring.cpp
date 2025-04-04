da716b3abf9888525aff8a749594b0dcfa499dd9
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-04-04 16:28:49 +0530

    Time: 1404 ms (5%), Space: 9.2 MB (94.26%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-    bool isPalindrome(string &s,int l,int r,vector<vector<int>> &dp)
+    bool isPalindrome(int start,int end,string& s)
     {
-        if(l>=r)
-            return 1;
-        if(dp[l][r]!=-1)
-            return dp[l][r];
-        if(s[l]!=s[r])
-            return 0;
-        return dp[l][r]=isPalindrome(s,l+1,r-1,dp);
+        while(start<=end)
+        {
+            if(s[start++]!=s[end--])
+            return false;
+        }
+        return true;
     }
     string longestPalindrome(string s) {
-        int maxLen=INT_MIN,starting=0;
-        vector<vector<int>>dp(s.size()+1,vector<int>(s.size()+1,-1));
-        for(int i=0;i<s.size();i++)
+        int start=-1,maxLen=0;
+        for(int i=0;i<s.length();i++)
         {
-            for(int j=i;j<s.size();j++)
+            for(int j=i;j<s.length();j++)
             {
-                if(isPalindrome(s,i,j,dp))
+                if(isPalindrome(i,j,s))
                 {
                     if(j-i+1>maxLen)
                     {
                         maxLen=j-i+1;
-                        starting=i;
+                        start=i;
                     }
                 }
             }
         }
-        return s.substr(starting,maxLen);
+        return s.substr(start,maxLen);
     }
 };
