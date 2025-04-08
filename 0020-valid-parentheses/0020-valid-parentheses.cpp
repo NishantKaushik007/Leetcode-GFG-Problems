6acc78250345659e8c137c6fb87d418276c8cdeb
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-04-08 23:14:45 +0530

    Time: 1 ms (16.41%), Space: 10 MB (7.73%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char>st;
+        stack<int>st;
         for(int i=0;i<s.length();i++)
         {
             if(st.empty()&&(s[i]==')'||s[i]=='}'||s[i]==']'))
-                return false;
+            return false;
             else if(s[i]=='('||s[i]=='{'||s[i]=='[')
-                st.push(s[i]);
-            else if(s[i]==')'&&st.top()=='(')
-                st.pop();
-            else if(s[i]=='}'&&st.top()=='{')
-                st.pop();
-            else if(s[i]==']'&&st.top()=='[')
-                st.pop();
+            st.push(s[i]);
+            else if(st.top()=='('&&s[i]==')')
+            st.pop();
+            else if(st.top()=='{'&&s[i]=='}')
+            st.pop();
+            else if(st.top()=='['&&s[i]==']')
+            st.pop();
             else
-                return false;
+            return false;
         }
         if(st.empty())
-            return true;
+        return true;
         return false;
     }
 };
