1621c20ff4b8de8069b93317c20d0e4fb6c1be8b
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-04-08 20:08:04 +0530

    Time: 0 ms (100%), Space: 15.2 MB (33.79%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -9,17 +9,22 @@ public:
             return mid;
             else if(nums[low]<=nums[mid])
             {
-                if(nums[low]<=target&&nums[mid]>=target)
-                high=mid-1;
-                else
-                low=mid+1;
-
+                if(target>=nums[low]&&target<=nums[mid])
+                {
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
             }
             else{
-                if(nums[mid]<=target&&nums[high]>=target)
-                low=mid+1;
-                else
-                high=mid-1;
+                if(target>=nums[mid]&&target<=nums[high])
+                {
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
             }
         }
         return -1;
