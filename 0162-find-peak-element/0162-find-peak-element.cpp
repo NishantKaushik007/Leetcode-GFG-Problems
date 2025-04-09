5869d59a74caf22415ff238df9190a150ea9f7d7
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-04-09 10:45:38 +0530

    Time: 0 ms (100%), Space: 12.5 MB (67.79%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -2,24 +2,22 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         if(nums.size()==1)
-            return 0;
+        return 0;
         if(nums[0]>nums[1])
-            return 0;
+        return 0;
         if(nums[nums.size()-1]>nums[nums.size()-2])
-            return nums.size()-1;
+        return nums.size()-1;
         int low=1,high=nums.size()-2;
         while(low<=high)
         {
             int mid=(low+high)/2;
-            if(nums[mid-1]<nums[mid]&&nums[mid]>nums[mid+1])
-                return mid;
-            //mid is on increasing curve and not peak so peak will be on right
+            if(nums[mid]>nums[mid-1]&&nums[mid]>nums[mid+1])
+            return mid;
             else if(nums[mid-1]<nums[mid])
-                low=mid+1;
-            //mid is on decreasing curve and not peak so peak will be on left
+            low=mid+1;
             else
-                high=mid-1;
+            high=mid-1;
         }
-        return -1;
+        return low;
     }
 };
