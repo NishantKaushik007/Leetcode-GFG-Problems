96ece4c4e74bf8781bdfa56611f0dbffb191e130
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-04-09 09:44:42 +0530

    Time: 0 ms (100%), Space: 8.9 MB (32.92%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        vector<int>nums;
         int fact=1;
+        vector<int>nums;
         for(int i=1;i<n;i++)
         {
             fact*=i;
             nums.push_back(i);
         }
         nums.push_back(n);
-        k=k-1;
         string ans="";
+        k=k-1;
         while(true)
         {
-            ans+=to_string(nums[k/fact]);
-            nums.erase(nums.begin()+k/fact);
+            string num=to_string(nums[k/fact]);
+            ans+=num;
+            nums.erase(nums.begin()+(k/fact));
             if(nums.size()==0)
-                break;
+            break;
             k=k%fact;
             fact=fact/nums.size();
         }
