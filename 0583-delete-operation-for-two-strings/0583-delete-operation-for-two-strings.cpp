2aa4eef63a5300df77c628a26c4f15fcb3404248
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-04-09 19:36:57 +0530

    Time: 11 ms (65.6%), Space: 16.8 MB (41.87%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -18,7 +18,22 @@ public:
     }
     int minDistance(string word1, string word2) {
         int n=word1.length(),m=word2.length();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return dis(n-1,m-1,word1,word2,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=m;i++)
+        dp[0][i]=i;
+        for(int i=1;i<=n;i++)
+        dp[i][0]=i;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                int match=1e9;
+                if(word1[i-1]==word2[j-1])
+                match=dp[i-1][j-1];
+                int notMatch=1+min(dp[i-1][j],dp[i][j-1]);
+                dp[i][j]=min(match,notMatch);
+            }
+        }
+        return dp[n][m];
     }
 };
