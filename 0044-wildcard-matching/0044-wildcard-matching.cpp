1be0c2c3e59d65e84ee79c0a798b49a8f9836613
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-04-09 17:49:59 +0530

    Time: 15 ms (89.06%), Space: 10.8 MB (82.95%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     bool isMatch(string s, string p) {
         int n=s.length(),m=p.length();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        dp[0][0]=1;
+        vector<int>prev(m+1,0),curr(m+1,0);
+        prev[0]=1;
         for(int i=1;i<=n;i++)
         {
-            dp[i][0]=0;
+            curr[0]=0;
         }
         for(int j=1;j<=m;j++)
         {
@@ -19,20 +19,21 @@ public:
                     break;
                 }
             }
-            dp[0][j]=flag;
+            prev[j]=flag;
         }
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=m;j++)
             {
                 if(s[i-1]==p[j-1]||p[j-1]=='?')
-                dp[i][j]=dp[i-1][j-1];
+                curr[j]=prev[j-1];
                 else if(p[j-1]=='*')
-                dp[i][j]=dp[i][j-1]|dp[i-1][j];
+                curr[j]=curr[j-1]|prev[j];
                 else
-                dp[i][j]=false;
+                curr[j]=false;
             }
+            prev=curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
