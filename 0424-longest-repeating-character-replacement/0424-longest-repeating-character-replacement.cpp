c8a7efd772822c8e839e7b6c25f2f0750cce3f68
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-04-12 18:59:59 +0530

    Time: 0 ms (100%), Space: 10.8 MB (93.14%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int freq[26]={0},l=0,r=0,maxLen=0,maxFreq=0;
+        int l=0,r=0,maxLen=0,hash[26]={0},maxFreq=0;
         while(r<s.length())
         {
-            freq[s[r]-'A']++;
-            maxFreq=max(maxFreq,freq[s[r]-'A']);
-            if((r-l+1)-maxFreq>k)
+            hash[s[r]-'A']++;
+            maxFreq=max(maxFreq,hash[s[r]-'A']);
+            while((r-l+1)-maxFreq>k)
             {
-                freq[s[l]-'A']--;
+                hash[s[l]-'A']--;
                 l++;
             }
-            if((r-l+1)-maxFreq<=k)
-                maxLen=max(maxLen,r-l+1);
+            maxLen=max(maxLen,r-l+1);
             r++;
         }
         return maxLen;
