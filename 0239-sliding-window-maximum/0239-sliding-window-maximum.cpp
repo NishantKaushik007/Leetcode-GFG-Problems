f879b7b66c037ee1f5a0c3295c4b0de77bd39823
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-04-19 18:46:49 +0530

    Time: 19 ms (78.61%), Space: 139.1 MB (83.84%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k){
-        deque<int> dq;
-        vector<int> res;
-        for (int i = 0; i < k; i++) {
-            while (!dq.empty() && nums[i] >= nums[dq.back()]) {
-                dq.pop_back();
-            }
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        //index
+        deque<int>dq;
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            while(!dq.empty()&&nums[dq.back()]<=nums[i])
+            dq.pop_back();
             dq.push_back(i);
+            if(dq.front()<=i-k)
+            dq.pop_front();
+            if(i>=k-1)
+            ans.push_back(nums[dq.front()]);
         }
-        res.push_back(nums[dq.front()]);
-
-        for (int i = k; i < nums.size(); i++) {
-            if(dq.front() == i - k) {
-                dq.pop_front();
-            }
-            while (!dq.empty() && nums[i] >= nums[dq.back()]) {
-                dq.pop_back();
-            }
-
-            dq.push_back(i);
-            res.push_back(nums[dq.front()]);
-        }
-        
-        return res;
+        return ans;
     }
 };
