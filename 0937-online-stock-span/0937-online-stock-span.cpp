4396c8c83b0245a3b66cc32ee1e9690560c173d0
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-05-15 00:31:08 +0530

    Time: 33 ms (51.22%), Space: 91.9 MB (88.6%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.cpp
+++ 0937-online-stock-span/0937-online-stock-span.cpp
@@ -1,6 +1,5 @@
 class StockSpanner {
 public:
-    //element,ind
     stack<pair<int,int>>st;
     int ind=0;
     StockSpanner() {
@@ -10,9 +9,9 @@ public:
     int next(int price) {
         while(!st.empty()&&st.top().first<=price)
         st.pop();
-        int ans=st.empty()?ind-(-1):ind-st.top().second;
+        int psee=st.empty()?ind-(-1):ind-st.top().second;
         st.push({price,ind++});
-        return ans;
+        return psee;
     }
 };
 
