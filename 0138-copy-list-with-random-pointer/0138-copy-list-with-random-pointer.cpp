5a7cddf23074cb2b1fb9fce3182c1d2575c65f6a
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-05-15 21:43:12 +0530

    Time: 5 ms (64.99%), Space: 14.9 MB (87.25%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -19,24 +19,23 @@ public:
     Node* copyRandomList(Node* head) {
         if(head==NULL)
         return NULL;
-        Node* curr=NULL;
+        Node* curr;
         for(curr=head;curr!=NULL;curr=curr->next->next)
         {
-            Node* temp=new Node(curr->val);
-            temp->next=curr->next;
-            curr->next=temp;
+            Node* node=new Node(curr->val);
+            node->next=curr->next;
+            curr->next=node;
         }
         for(curr=head;curr!=NULL;curr=curr->next->next)
-        curr->next->random=(curr->random)?curr->random->next:NULL;
-        Node* copy=head->next,*original=head;
-        curr=copy;
-        while(copy&&original)
+        curr->next->random=curr->random?curr->random->next:NULL;
+        Node* original=head,*copy=head->next,*temp=copy;
+        while(original)
         {
-            original->next=(original->next->next)?original->next->next:NULL;
+            original->next=original->next->next?original->next->next:NULL;
             original=original->next;
-            copy->next=(copy->next)?copy->next->next:NULL;
+            copy->next=copy->next?copy->next->next:NULL;
             copy=copy->next;
         }
-        return curr;
+        return temp;
     }
 };
