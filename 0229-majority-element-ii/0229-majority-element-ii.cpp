ec889f971dee4295507000373d6428b28b33e90d
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-05-19 21:54:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int ele1=-1,ele2=-1,c1=0,c2=0;
+        vector<int>ans;
+        int ele1=-1,ele2=-1,count1=0,count2=0;
         for(int i=0;i<nums.size();i++)
         {
-            if(ele1==nums[i])
-            c1++;
-            else if(ele2==nums[i])
-            c2++;
-            else if(c1==0)
+            if(count1==0)
             {
                 ele1=nums[i];
-                c1=1;
+                count1=1;
             }
-            else if(c2==0)
+            else if(count2==0)
             {
                 ele2=nums[i];
-                c2=1;
+                count2=1;
             }
+            if(nums[i]==ele1)
+            count1++;
+            else if(nums[i]==ele2)
+            count2++;
             else{
-                c1--;
-                c2--;
+                count1--;
+                count2--;
             }
         }
-        c1=0,c2=0;
+        count1=0,count2=0;
         for(int i=0;i<nums.size();i++)
         {
             if(nums[i]==ele1)
-            c1++;
+            count1++;
             else if(nums[i]==ele2)
-            c2++;
+            count2++;
         }
-        vector<int>ans;
-        if(c1>nums.size()/3)
+        if(count1>(nums.size()/3))
         ans.push_back(ele1);
-        if(c2>nums.size()/3)
+        if(count2>(nums.size()/3))
         ans.push_back(ele2);
         return ans;
     }
