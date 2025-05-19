d508556fe656b0835734d48a8a93e2b098a822ea
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-05-19 14:25:00 +0530

    Time: 4 ms (69.92%), Space: 23.8 MB (59.26%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -8,7 +8,7 @@ public:
             if(ans.size()==0||ans.back()[1]<intervals[i][0])
             ans.push_back(intervals[i]);
             else
-            ans.back()[1]=max(intervals[i][1],ans.back()[1]);
+            ans.back()[1]=max(ans.back()[1],intervals[i][1]);
         }
         return ans;
     }
