39d39a4bfe05de1d4d16cbd1f3a92d8843b1c53d
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-05-19 16:09:32 +0530

    Time: 0 ms (100%), Space: 13.3 MB (76.25%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,12 +4,12 @@ public:
         int ru=0,rd=matrix.size()-1,cl=0,cr=matrix[0].size()-1;
         while(ru<=rd&&cl<=cr)
         {
-            if(matrix[rd][cl]==target)
+            if(matrix[ru][cr]==target)
             return true;
-            else if(matrix[rd][cl]<target)
-            cl++;
+            else if(matrix[ru][cr]<target)
+            ru++;
             else
-            rd--;
+            cr--;
         }
         return false;
     }
