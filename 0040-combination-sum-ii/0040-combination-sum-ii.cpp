0d86e04a27d1f95be738e598197db92b5d2ab11b
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-05-28 16:22:30 +0530

    Time: 7 ms (13.79%), Space: 14 MB (60.91%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void combinationSum(int ind,int target,vector<int>& candidates,vector<int>& ds,vector<vector<int>>& ans)
+    void solve(int ind,vector<int>& candidates,int target,vector<int>& ds,vector<vector<int>>& ans)
     {
         if(target==0)
         {
@@ -11,10 +11,10 @@ public:
         {
             if(i!=ind&&candidates[i]==candidates[i-1])
             continue;
-            if(target>=candidates[i])
+            if(candidates[i]<=target)
             {
                 ds.push_back(candidates[i]);
-                combinationSum(i+1,target-candidates[i],candidates,ds,ans);
+                solve(i+1,candidates,target-candidates[i],ds,ans);
                 ds.pop_back();
             }
         }
@@ -23,7 +23,7 @@ public:
         vector<vector<int>>ans;
         vector<int>ds;
         sort(candidates.begin(),candidates.end());
-        combinationSum(0,target,candidates,ds,ans);
+        solve(0,candidates,target,ds,ans);
         return ans;
     }
 };
