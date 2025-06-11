27f9aff34fb0bdccf04e5a9ae3db2eb82b8a1b65
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-06-11 23:47:57 +0530

    Time: 15 ms (24.99%), Space: 20.6 MB (29.55%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n=heights.size(),largest=0;
-        vector<int>pse(n),nse(n),width(n);
+        int largestRectangle=0,n=heights.size();
         stack<int>st;
+        vector<int>nse(n),pse(n);
         for(int i=0;i<n;i++)
         {
             while(!st.empty()&&heights[st.top()]>=heights[i])
@@ -21,29 +21,32 @@ public:
             st.push(i);
         }
         for(int i=0;i<n;i++)
-        width[i]=(nse[i]-pse[i])-1;
-        for(int i=0;i<n;i++)
-        largest=max(largest,heights[i]*width[i]);
-        return largest;
+        {
+            largestRectangle=max(largestRectangle,(nse[i]-pse[i]-1)*heights[i]);
+        }
+        return largestRectangle;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
         int n=matrix.size(),m=matrix[0].size(),maxRectangle=0;
-        vector<vector<int>>prefixMat(n,vector<int>(m,-1));
+        vector<vector<int>>mat(n,vector<int>(m,0));
         for(int i=0;i<m;i++)
-        prefixMat[0][i]=matrix[0][i]-'0';
-        for(int j=0;j<m;j++)
         {
-            for(int i=1;i<n;i++)
+            if(matrix[0][i]=='1')
+            mat[0][i]=1;
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
             {
-                if(matrix[i][j]=='0')
-                prefixMat[i][j]=0;
-                else
-                prefixMat[i][j]=prefixMat[i-1][j]+1;
+                if(matrix[i][j]=='1')
+                {
+                    mat[i][j]=mat[i-1][j]+1;
+                }
             }
         }
         for(int i=0;i<n;i++)
         {
-            maxRectangle=max(maxRectangle,largestRectangleArea(prefixMat[i]));
+            maxRectangle=max(maxRectangle,largestRectangleArea(mat[i]));
         }
         return maxRectangle;
     }
