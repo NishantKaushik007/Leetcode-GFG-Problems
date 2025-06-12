eda1bf581eae0761b6ae54e15041d7df77629592
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-06-12 16:56:05 +0530

    Time: 4 ms (19.87%), Space: 15.1 MB (17.17%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        vector<int>prev(matrix[0].size(),-1);
-        for(int j=0;j<matrix[0].size();j++)
-        prev[j]=matrix[0][j];
-        for(int i=1;i<matrix.size();i++)
+        int mini=1e9,n=matrix.size(),m=matrix[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        for(int i=0;i<m;i++)
+        dp[0][i]=matrix[0][i];
+        for(int i=1;i<n;i++)
         {
-            vector<int>curr(matrix[0].size(),-1);
-            for(int j=0;j<matrix[0].size();j++)
+            for(int j=0;j<m;j++)
             {
-                int left=1e8;
-                if(j-1>=0)
-                left=matrix[i][j]+prev[j-1];
-                int down=matrix[i][j]+prev[j];
-                int right=1e8;
-                if(j+1<matrix[0].size())
-                right=matrix[i][j]+prev[j+1];
-                curr[j]=min(left,min(down,right));
+                int dl=1e9;
+                if(j>0)
+                dl=matrix[i][j]+dp[i-1][j-1];
+                int d=matrix[i][j]+dp[i-1][j];
+                int dr=1e9;
+                if(j+1<m)
+                dr=matrix[i][j]+dp[i-1][j+1];
+                dp[i][j]=mini=min(dl,min(d,dr));
             }
-            prev=curr;
         }
-        int mini=1e8;
-        for(int i=0;i<matrix[0].size();i++)
-        mini=min(mini,prev[i]);
+        for(int i=0;i<m;i++)
+        mini=min(mini,dp[m-1][i]);
         return mini;
     }
 };
