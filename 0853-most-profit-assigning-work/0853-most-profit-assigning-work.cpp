090f35c73c77cad51af76d3692a59791eaf0f536
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-06-27 22:31:42 +0530

    Time: 6 ms (88.82%), Space: 41.6 MB (60.59%) - LeetHub

--- 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        int maxProfit=0;
+        int n=profit.size(),maxProfit=0,totProfit=0;
         vector<pair<int,int>>v;
-        for(int i=0;i<difficulty.size();i++)
+        for(int i=0;i<n;i++)
         v.push_back({difficulty[i],profit[i]});
         sort(v.begin(),v.end());
-        for(int i=1;i<v.size();i++)
+        for(int i=1;i<n;i++)
         v[i].second=max(v[i-1].second,v[i].second);
         sort(worker.begin(),worker.end());
-        int j=0,mprofit=0;
+        int j=0;
         for(int i=0;i<worker.size();i++)
         {
-            while(worker[i]>=v[j].first&&j<v.size())
+            while(j<n&&worker[i]>=v[j].first)
             {
-                mprofit=max(mprofit,v[j].second);
+                maxProfit=max(maxProfit,v[j].second);
                 j++;
             }
-            maxProfit+=mprofit;
+            totProfit+=maxProfit;
         }
-        return maxProfit;
+        return totProfit;
     }
 };
