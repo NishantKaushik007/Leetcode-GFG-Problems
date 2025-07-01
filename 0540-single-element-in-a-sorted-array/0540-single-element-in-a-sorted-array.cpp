da0c4a91fb61f1dbf1f86ee0c99b7e109967b8a7
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-07-02 00:00:38 +0530

    Time: 0 ms (100%), Space: 26.3 MB (33.39%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -5,7 +5,7 @@ public:
         while(low<=high)
         {
             int mid=(low+high)/2;
-            if(nums[mid^1]==nums[mid])
+            if(nums[mid]==nums[mid^1])
             low=mid+1;
             else
             high=mid-1;
