31efc9d6bb6a45e5251482a69c8c19c490995dd8
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-07-02 00:12:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -3,21 +3,21 @@ public:
     int findPeakElement(vector<int>& nums) {
         if(nums.size()==1)
         return 0;
-        if(nums[0]>nums[1])
-        return 0;
-        if(nums[nums.size()-1]>nums[nums.size()-2])
-        return nums.size()-1;
-        int low=1,high=nums.size()-2;
+        int low=0,high=nums.size()-1;
         while(low<=high)
         {
             int mid=(low+high)/2;
             if(nums[mid]>nums[mid-1]&&nums[mid]>nums[mid+1])
             return mid;
-            else if(nums[mid-1]<nums[mid])
-            low=mid+1;
-            else
-            high=mid-1;
+            else if(nums[mid]>nums[mid-1])
+            {
+                low=mid+1;
+            }
+            else if(nums[mid]>nums[mid+1])
+            {
+                high=mid-1;
+            }
         }
-        return low;
+        return -1;
     }
 };
