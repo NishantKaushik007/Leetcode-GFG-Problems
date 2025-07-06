6f21396a62f24dde2e29b7b50e7324c172c5abdc
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-07-07 00:56:42 +0530

    Time: 7 ms (51.23%), Space: 17.8 MB (75.21%) - LeetHub

--- 0341-flatten-nested-list-iterator/0341-flatten-nested-list-iterator.cpp
+++ 0341-flatten-nested-list-iterator/0341-flatten-nested-list-iterator.cpp
@@ -17,37 +17,42 @@
  */
 
 class NestedIterator {
+    queue<int>q;
 public:
-    NestedIterator(vector<NestedInteger> &nestedList) {
-        begins.push(nestedList.begin());
-        ends.push(nestedList.end());
-    }
 
-    int next() {
-        hasNext();
-        return (begins.top()++)->getInteger();
-    }
-
-    bool hasNext() {
-        while (begins.size()) {
-            if (begins.top() == ends.top()) {
-                begins.pop();
-                ends.pop();
-            } else {
-                auto x = begins.top();
-                if (x->isInteger())
-                    return true;
-                begins.top()++;
-                begins.push(x->getList().begin());
-                ends.push(x->getList().end());
+    void flatten(vector<NestedInteger> &nestedList)
+    {
+        int n=nestedList.size();
+        for(int i=0;i<n;i++)
+        {
+            NestedInteger& obj=nestedList[i];
+            if(obj.isInteger())
+            q.push(obj.getInteger());
+            else{
+                flatten(obj.getList());
             }
         }
-        return false;
     }
 
-private:
-    stack<vector<NestedInteger>::iterator> begins, ends;
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        flatten(nestedList);
+    }
+    
+    int next() {
+        if(q.empty())
+        return -1;
+        int element=q.front();
+        q.pop();
+        return element;
+    }
+    
+    bool hasNext() {
+        if(q.empty())
+        return false;
+        return true;
+    }
 };
+
 /**
  * Your NestedIterator object will be instantiated and called as such:
  * NestedIterator i(nestedList);
