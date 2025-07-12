b6f6a0576937b47afecbc8b424e9071be6081590
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-07-12 20:26:25 +0530

    Time: 2 ms (3.5%), Space: 11.5 MB (97.41%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -4,12 +4,13 @@ public:
         int low=0,mid=0,high=nums.size()-1;
         while(mid<=high)
         {
-            if(nums[mid]==0)
-            swap(nums[low++],nums[mid++]);
-            else if(nums[mid]==1)
+            if(nums[mid]==1)
             mid++;
-            else if(nums[mid]==2)
-            swap(nums[high--],nums[mid]);
+            else if(nums[mid]==0)
+            swap(nums[low++],nums[mid++]);
+            else{
+                swap(nums[mid],nums[high--]);
+            }
         }
     }
 };
