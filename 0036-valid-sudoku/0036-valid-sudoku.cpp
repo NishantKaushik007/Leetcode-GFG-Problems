91230cf8875ba2c921349a372a939de1eeb7d47f
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-08-30 23:30:16 +0530

    Time: 0 ms (100%), Space: 21.6 MB (91.85%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for(int row=0;row<9;row++)
+        {
+            for(int col=0;col<9;col++)
+            {
+                if(board[row][col]!='.')
+                {
+                    char c=board[row][col];
+                    board[row][col]='.';
+                    for(int i=0;i<9;i++)
+                    {
+                        if(board[row][i]==c)
+                            return false;
+                        if(board[i][col]==c)
+                            return false;
+                        if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c)
+                            return false;
+                    }
+                    board[row][col]=c;
+                }
+            }
+        }
+        return true;
+    }
+};
